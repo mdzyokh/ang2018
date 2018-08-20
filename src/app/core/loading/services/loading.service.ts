b179e0dd6fc9df4a0888a9d6f3a91c796b77c935
Mariia_Ponomaryova <Mariia_Ponomaryova@epam.com>
2018-08-20 11:06:43 +0200

    fix issue with loading page does not hide

--- src/app/core/loading/services/loading.service.ts
+++ src/app/core/loading/services/loading.service.ts
@@ -1,6 +1,7 @@
 import { ApplicationRef, ComponentFactoryResolver, Injectable, Injector } from '@angular/core';
-import { DomPortalHost, Portal, ComponentPortal } from '@angular/cdk/portal';
+import { DomPortalHost, ComponentPortal } from '@angular/cdk/portal';
 import { LoadingComponent } from '../loading.component';
+import { BehaviorSubject } from 'rxjs';
 
 @Injectable({
     providedIn: 'root',
@@ -12,19 +13,16 @@ export class LoadingService {
     constructor(
         private componentFactoryResolver: ComponentFactoryResolver,
         private injector: Injector,
-        private appRef: ApplicationRef
-    ) { }
-
-    show() {
-        if (this.portal.isAttached) {
-            return;
-        }
+        private appRef: ApplicationRef) {
         this.portalHost = new DomPortalHost(
             document.querySelector('body'),
             this.componentFactoryResolver,
             this.appRef,
             this.injector
         );
+    }
+
+    show() {
         this.portalHost.attach(this.portal);
     }
 
